5d8b6ff2546de67f7afdb9c6c99a4a8ed229ccc7
Michael Geary <mgeary1994@gmail.com>
2025-12-16 20:20:10 +0000

    add logging to bingo app and add configs to make it pick up sql (maybe)

--- src/main/java/com/example/bingo/BingoApplication.java
+++ src/main/java/com/example/bingo/BingoApplication.java
@@ -1,5 +1,9 @@
 package com.example.bingo;
 
+import com.example.bingo.repository.BingoPhraseRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +13,14 @@ public class BingoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BingoApplication.class, args);
 	}
+
+	@Bean
+    public CommandLineRunner checkPhrases(BingoPhraseRepository repo) {
+        return args -> {
+            long count = repo.count();
+            System.out.println("Bingo phrases loaded in DB: " + count);
+            repo.findAll().forEach(p -> System.out.println(p.getPhrase() + " (" + p.getType() + ")"));
+        };
+    }
 
 }
